75bafcbdf90070233aaf38bbfc1c2dbfd6a97883
bhuwanmahato <bhuwan8501@gmail.com>
2025-06-26 11:16:46 +0530

    activation key

---
+++ src/app/activation-key/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ActivationKey = () => {
+  const data = {
+    message:
+      "Offer 🔥 60% Off. Unlock full features. Get custom colors, new animations, and interactions. Enter license key to continue.",
+  };
+
+  return (
+    <div>
+      <p>{data.message}</p>
+    </div>
+  );
+};
+
+export default ActivationKey;
